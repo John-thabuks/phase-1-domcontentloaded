d136c2a69932f76adec0104eb1bb044d64fabdc4
John-thabuks <thabuksjohn@gmail.com>
2023-10-14 20:18:54 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded",function(){
+    let textAfterDomLad = document.querySelector("p");
+    textAfterDomLad.textContent = "This is really cool!"
+    return textAfterDomLad;
+})
